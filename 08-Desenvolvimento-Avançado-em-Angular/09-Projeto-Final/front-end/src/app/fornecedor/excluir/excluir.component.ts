bae5282f1f6d87329548060cff8b6ac857a468d3
Daiane Cordeiro <67031248+daiccordeiro@users.noreply.github.com>
2026-03-20 22:47:40 -0300

    Exclusão de Fornecedor

--- 08-Desenvolvimento-Avançado-em-Angular/09-Projeto-Final/front-end/src/app/fornecedor/excluir/excluir.component.ts
+++ 08-Desenvolvimento-Avançado-em-Angular/09-Projeto-Final/front-end/src/app/fornecedor/excluir/excluir.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 import { ToastrService } from 'ngx-toastr';
+
+import { NgxBrazil } from 'ngx-brazil';
 
 import { FornecedorService } from '../services/fornecedor.service';
 import { Fornecedor } from '../models/fornecedor';
@@ -16,30 +20,38 @@ import { Fornecedor } from '../models/fornecedor';
     imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    NgxBrazil
   ],
   templateUrl: './excluir.component.html'
 })
-export class ExcluirComponent implements OnInit {
-  fornecedor!: Fornecedor;
+export class ExcluirComponent {
 
   private fornecedorService = inject(FornecedorService);
   private router = inject(Router);
   private toastr = inject(ToastrService);
   private route = inject(ActivatedRoute);
+  private sanitizer = inject(DomSanitizer);
 
-  ngOnInit(): void {
-    //const id = this.route.snapshot.paramMap.get('id');
-    this.fornecedor = this.route.snapshot.data['fornecedor'];
+  readonly fornecedor$: Observable<Fornecedor> =
+    this.route.data.pipe(
+      map(data => data['fornecedor'])
+  );
 
-    if (!this.fornecedor) {
-      this.router.navigate(['/fornecedores/listar-todos']);
-    }
-  }
+  readonly enderecoMap$: Observable<SafeResourceUrl> =
+    this.fornecedor$.pipe(
+      map(f => {
+        const enderecoCompleto = `${f.endereco.logradouro}, ${f.endereco.numero} - ${f.endereco.bairro}, ${f.endereco.cidade} - ${f.endereco.estado}`;
 
-  excluirEvento(): void {
+        const url = `https://www.google.com/maps/embed/v1/place?q=${enderecoCompleto}&key=AIzaSyAP0WKpL7uTRHGKWyakgQXbW6FUhrrA5pE`;
+
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+      })
+  );
+
+  excluirEvento(Fornecedor: Fornecedor): void {
     this.fornecedorService
-      .excluirFornecedor(this.fornecedor.id)
+      .excluirFornecedor(Fornecedor.id)
       .subscribe({
         next: () => this.sucessoExclusao(),
         error: () => this.falha()
